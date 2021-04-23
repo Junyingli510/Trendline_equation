7e081647dc5a48d7fae5effb0c3c03f445288801
Tao Hong <76576903+Junyingli510@users.noreply.github.com>
2021-04-24 02:44:23 +0800

    Add files via upload

---
+++ trend line equation.py
@@ -0,0 +1,56 @@
+from numpy import arange
+from pandas import read_csv
+from scipy.optimize import curve_fit
+from matplotlib import pyplot
+ 
+# define the true objective function
+def objective(x, a, b):
+	return a * x + b
+
+data = [
+        [10, 15.4], 
+		[19, 19.4], 
+		[28, 10.8], 
+		[37, 30.2], 
+		[46, 11.7], 
+		[55, 49.3],
+		[64, 8.86], 
+		[73, 61.1], 
+		[82, 69.3], 
+		[91, 75]
+        ]
+x = []
+y = []
+
+for i in range(0, len(data)):
+    value = data[i]
+    x.append(value[0])
+    y.append(value[1])
+
+print(x)
+print(y)
+
+# # load the dataset
+# url = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/longley.csv'
+# dataframe = read_csv(url, header=None)
+# data = dataframe.values
+# # choose the input and output variables
+# x, y = data[:, 4], data[:, -1]
+# print(x)
+# print(y)
+# curve fit
+popt, _ = curve_fit(objective, x, y)
+# summarize the parameter values
+a, b = popt
+print('y = %.5f * x + %.5f' % (a, b))
+# plot input vs output
+pyplot.scatter(x, y)
+# define a sequence of inputs between the smallest and largest known inputs
+x_line = arange(min(x), max(x), 1)
+# calculate the output for the range
+y_line = objective(x_line, a, b)
+# create a line plot for the mapping function
+title = 'Trend Line Equation: y = %.5f * x + %.5f' % (a, b)
+pyplot.plot(x_line, y_line, '--', color='red')
+pyplot.title(title)
+pyplot.show()
